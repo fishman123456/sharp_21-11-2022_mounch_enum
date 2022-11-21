3f4f4b601c5a3b01782df2518e1037369b4fd0d8
F_D <den9152@yandex.ru>
2022-11-21 20:51:05 +0700

    Initial commit

---
+++ Class1.cs
@@ -0,0 +1,34 @@
+﻿enum MyEnum
+{
+    January = 1,
+    February = 2,
+    March = 3,
+    April = 4,
+    May = 5,
+    June = 6,
+    July = 7,
+    August = 8,
+    September = 9,
+    October = 10,
+    November = 11,
+    December = 12
+}
+class Count_
+{
+    public delegate void Count_one(MyEnum co);
+
+    public event Count_one Co_ev;
+
+
+    public void circle(MyEnum v1)
+    {
+
+        for (MyEnum i = MyEnum.January; i <= MyEnum.December; i++)
+        {
+            if ((int) i % 3 ==0)
+            {
+                Co_ev(co);
+            }
+        }
+    }
+}
